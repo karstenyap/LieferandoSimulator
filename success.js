cb161d8491c4c2b2d75fe50d489505e9c6fb8d07
Nisthegoat <nurinira96@gmail.com>
2024-12-31 16:51:32 +0100

    Create success.js

---
+++ success.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headerElement = document.getElementById("header");
+
+    // Make the header visible after the page loads
+    headerElement.style.visibility = 'visible';
+});
