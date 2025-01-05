cfea8e9e92f951b05913fdbecf0b9a2972d9ab83
Nisthegoat <nurinira96@gmail.com>
2025-01-05 17:49:20 +0100

    Create edit_menu.js

---
+++ edit_menu.js
@@ -0,0 +1,14 @@
+// JavaScript to preview the image when selected
+function previewImage(input, previewId) {
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const preview = document.getElementById(previewId);
+        preview.src = e.target.result;
+    };
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
