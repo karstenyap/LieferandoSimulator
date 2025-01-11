bfa2b2bd387de1a045710d13597f450c84b502c7
Nisthegoat <nurinira96@gmail.com>
2025-01-11 16:50:10 +0100

    Update edit_menu.js

--- edit_menu.js
+++ edit_menu.js
@@ -12,3 +12,28 @@ function previewImage(input, previewId) {
         reader.readAsDataURL(file);
     }
 }
+
+function deleteMenuItem(button) {
+    var itemId = button.getAttribute('data-item-id');
+
+    fetch('/delete_menu_item/' + itemId, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            const notification = document.getElementById('notification');
+            notification.style.display = 'block';
+            notification.innerHTML = `<p>${data.message} Please return to the <a href="/menu_management">menu management</a> page.</p>`;
+        } else {
+            alert('Failed to delete menu item.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while deleting the menu item.');
+    });
+}
